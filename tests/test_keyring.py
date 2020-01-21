8332fc24005a4e3e59d901b483e3081293873186
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-21 18:32:01 -0500

    test: Clean up test case

--- tests/test_keyring.py
+++ tests/test_keyring.py
@@ -16,18 +16,21 @@ def alice():
 
 @pytest.fixture
 def bob():
-    return KeyringPair(BOB, "ed25519")
+    return KeyringPair(BOB, key_type="ed25519")
 
 
 def test_pairs(alice, bob):
     keyring = Keyring()  # Defaults to ed25519
 
-    pair = keyring.add_from_uri(ALICE)
+    keyring.add_from_uri(ALICE)
+    pair = keyring.pairs[-1]
     assert pair == alice
     assert len(keyring.pairs) == 1
     assert pair in keyring.pairs
 
-    pair = keyring.add_from_uri(BOB)
+    keyring.add_from_uri(BOB)
+    pair = keyring.pairs[-1]
     assert pair == bob
-    assert pair in keyring.pairs
     assert len(keyring.pairs) == 2
+    assert pair in keyring.pairs
+    assert alice in keyring.pairs
